4b562edbd371b6f8230082c85a51843c27b11661
papinianus <dglmsksooah@gmail.com>
2018-06-15 16:08:08 +0900

    https://teratail.com/questions/131217

---
+++ csv/csv/BodyMass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csv
+{
+    public sealed class BodyMass
+    {
+        public BodyMass(string csvrow)
+        {
+            var splitttedrow = csvrow.Split(',').Select(x => x.Trim());
+            ID = splitttedrow.First();
+            Weights = splitttedrow.Skip(1).Select(x => double.Parse(x));
+            Diffs = Weights.Zip(Weights.Skip(1), (first, second) => Math.Round(second - first, 1));
+        }
+        public string ID { get; private set; } = "0000";
+        public IEnumerable<double> Weights { get; private set; } = new double[0];
+        public IEnumerable<double> Diffs { get; private set; } = new double[0];
+
+        public override string ToString() => $"{ID},{string.Join(",", Weights)},{string.Join(",", Diffs)}";
+    }
+}
---
+++ csv/csv/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace csv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var file = "TestData.csv";//@"c:\~\~\~\TestData.csv";
+            var dat = File.ReadAllLines(file);
+            Console.WriteLine(Header(dat.First()));
+            foreach(var id in dat.Skip(1).Select(x => new BodyMass(x)))
+            {
+                Console.WriteLine(id.ToString());
+            }
+            Console.ReadKey();
+        }
+        private static string Header(string origialHeader)
+        {
+            var items = origialHeader.Split(',').Select(x => x.Trim());
+            return $"{items.First()},{string.Join(",", items.Skip(1))},{string.Join(",", items.Skip(1).Zip(items.Skip(2), (first, second) => $"{first}と{second}との差"))}";
+        }
+    }
+}
